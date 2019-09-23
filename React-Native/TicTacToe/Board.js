99d665edb2dbeb742482a3aff54205e8eabf2ef3
ChoiHyeongu <a11dsd123@gmail.com>
2019-09-24 00:19:01 +0900

    Tic Tac Toe

--- React-Native/TicTacToe/Board.js
+++ React-Native/TicTacToe/Board.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Button from './Button.js';
 
 const CIRCLE = "O";
 const CROSS = "X";
-const NOTHING = " ";
+const NOTHING = "   ";
 
 class Board extends React.Component {
 
     constructor(props) {
         super(props);
 
+        console.log("constructor");
         this.checkBoard = this.checkBoard.bind(this);
+        this.setCell = this.setCell.bind(this)
 
         this.state = {
             gameState: [],
@@ -28,46 +30,100 @@ class Board extends React.Component {
         return (
             <View style={styles.board}>
                 <View style={styles.row}>
-                    <Button turn={CROSS} onPress={this.checkBoard.bind(this, 0, 0)} />
-                    <Button style={styles.borderStartEnd} turn={CROSS} onPress={this.checkBoard.bind(this, 0, 1)} />
-                    <Button turn={CROSS} onPress={this.checkBoard.bind(this, 0, 2)} />
+                    <Button turn={this.setCell(0)} onPress={this.checkBoard.bind(this, 0)} />
+                    <Button style={styles.borderStartEnd} turn={this.setCell(1)} onPress={this.checkBoard.bind(this, 1)} />
+                    <Button turn={this.setCell(2)} onPress={this.checkBoard.bind(this, 2)} />
                 </View>
-                <View style={[styles.row, { borderTopWidth: 1, borderBottomWidth: 1}]}>
-                    <Button turn={CROSS} onPress={this.checkBoard.bind(this, 1, 0)} />
-                    <Button style={styles.borderStartEnd} turn={CROSS} onPress={this.checkBoard.bind(this, 1, 1)} />
-                    <Button turn={CROSS} onPress={this.checkBoard.bind(this, 1, 2)} />
+                <View style={[styles.row, { borderTopWidth: 1, borderBottomWidth: 1 }]}>
+                    <Button turn={this.setCell(3)} onPress={this.checkBoard.bind(this, 3)} />
+                    <Button style={styles.borderStartEnd} turn={this.setCell(4)} onPress={this.checkBoard.bind(this, 4)} />
+                    <Button turn={this.setCell(5)} onPress={this.checkBoard.bind(this, 5)} />
                 </View>
                 <View style={styles.row}>
-                    <Button turn={CROSS} onPress={this.checkBoard.bind(this, 2, 0)} />
-                    <Button style={styles.borderStartEnd} turn={CROSS} onPress={this.checkBoard.bind(this, 2, 1)} />
-                    <Button turn={CROSS} onPress={this.checkBoard.bind(this, 2, 2)} />
+                    <Button turn={this.setCell(6)} onPress={this.checkBoard.bind(this, 6)} />
+                    <Button style={styles.borderStartEnd} turn={this.setCell(7)} onPress={this.checkBoard.bind(this, 7)} />
+                    <Button turn={this.setCell(8)} onPress={this.checkBoard.bind(this, 8)} />
                 </View>
             </View>
         );
     }
 
-    checkBoard(row, col) {
-        
-        var currentPlayer = this.state.currenPlayer;
-        
-        var arr = this.state.gameState.slice();
-        arr[row, col] = currentPlayer;
+    checkBoard(index) {
+
+        var currentPlayer = this.state.currentPlayer;
+        console.log(`[${index}] = ${currentPlayer}`);
+
+        var arr = this.state.gameState;
+        arr[index] = currentPlayer;
         this.setState({ gameState: arr });
+        
+        this.checkWinner(arr);
 
         var nextPlayer = (currentPlayer == 1) ? -1 : 1;
-        this.setState({currentPlayer: nextPlayer});
-        
+        this.setState({ currentPlayer: nextPlayer });
+
         console.log(this.state.gameState);
+        console.log("---------------");
+    }
+
+    setCell(index){
+        var value = this.state.gameState[index];
+        switch (value) {
+            case 1:
+                return CIRCLE;
+                break;
+            case -1:
+                return CROSS;
+                break;
+            default :
+                return NOTHING;
+                break;
+        }
     }
 
     initializeGame() {
         this.setState({
             gameState: [
-                [0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]
             ]
         });
+    }
+
+    checkWinner(gameState){
+
+        const winCase = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        var arr = [];
+
+        for(let i=0;i<winCase.length;i++){
+            const [a, b, c] = winCase[i];
+            if(gameState[a] && gameState[a] === gameState[b] && gameState[b] === gameState[c]){
+                console.log("Winner is " + this.state.currentPlayer);
+                Alert.alert(
+                    `Winner is ${this.state.currentPlayer}`,
+                    'If you want play more press OK',
+                    [
+                      {
+                        text: 'OK',
+                        onPress: () => console.log('Cancel Pressed'),
+                        style: 'cancel',
+                      }
+                    ],
+                  );
+                break;
+            } else {
+                console.log("Nobody win " + gameState[a] +", " + gameState[b] + ", " + gameState[c]);
+            }
+        }
     }
 }
 
